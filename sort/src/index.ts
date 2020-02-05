7563fa7e7ef6293f11a7b555c0cd0639e4febd60
hsimao <e087754958@gmail.com>
2020-02-05 21:20:21 +0800

    add: sort app

---
+++ sort/src/index.ts
@@ -0,0 +1,22 @@
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j]
+          this.collection[j] = this.collection[j + 1]
+          this.collection[j + 1] = leftHand
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 4, 0, 3, -4])
+sorter.sort()
+
+console.log(sorter.collection)
