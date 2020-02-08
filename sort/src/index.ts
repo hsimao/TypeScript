6479641d50b419248158cacc86ef2ee7dc2ca89f
hsimao <e087754958@gmail.com>
2020-02-08 13:58:53 +0800

    add: [sort] - 使用抽象 abstract 替換 interface 建立 class 關聯

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,26 +1,19 @@
-import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollection'
 import { LinkedList } from './LinkedList'
 
 const numbersCollection = new NumbersCollection([-10, 4, 0, 3, -4])
-const charactersCollection = new CharactersCollection('ZZaZhbYC')
+numbersCollection.sort()
+console.log(numbersCollection.data)
 
-const sorter = new Sorter(numbersCollection)
-const stringSorter = new Sorter(charactersCollection)
-
-sorter.sort()
-stringSorter.sort()
-
-// console.log(numbersCollection.data)
-// console.log(charactersCollection.data)
+const charactersCollection = new CharactersCollection('cdabuzE')
+charactersCollection.sort()
+console.log(charactersCollection.data)
 
 const linkedList = new LinkedList()
 linkedList.add(500)
 linkedList.add(-10)
 linkedList.add(-3)
 linkedList.add(4)
-
-const sorterList = new Sorter(linkedList)
-sorterList.sort()
+linkedList.sort()
 linkedList.print()
