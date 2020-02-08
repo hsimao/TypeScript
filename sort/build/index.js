d1b0a5662b5c16fd3027d2a52ee15e5bb44f8abd
hsimao <e087754958@gmail.com>
2020-02-08 12:57:44 +0800

    add: LinkedList

--- sort/build/index.js
+++ sort/build/index.js
@@ -3,11 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
 var CharactersCollection_1 = require("./CharactersCollection");
+var LinkedList_1 = require("./LinkedList");
 var numbersCollection = new NumbersCollection_1.NumbersCollection([-10, 4, 0, 3, -4]);
-var charactersCollection = new CharactersCollection_1.CharactersCollection('ZZaZbYC');
+var charactersCollection = new CharactersCollection_1.CharactersCollection('ZZaZhbYC');
 var sorter = new Sorter_1.Sorter(numbersCollection);
 var stringSorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
 stringSorter.sort();
-console.log(numbersCollection.data);
-console.log(charactersCollection.data);
+// console.log(numbersCollection.data)
+// console.log(charactersCollection.data)
+var linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+var sorterList = new Sorter_1.Sorter(linkedList);
+sorterList.sort();
+linkedList.print();
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollection'
+import { LinkedList } from './LinkedList'
 
 const numbersCollection = new NumbersCollection([-10, 4, 0, 3, -4])
 const charactersCollection = new CharactersCollection('ZZaZhbYC')
@@ -11,5 +12,15 @@ const stringSorter = new Sorter(charactersCollection)
 sorter.sort()
 stringSorter.sort()
 
-console.log(numbersCollection.data)
-console.log(charactersCollection.data)
+// console.log(numbersCollection.data)
+// console.log(charactersCollection.data)
+
+const linkedList = new LinkedList()
+linkedList.add(500)
+linkedList.add(-10)
+linkedList.add(-3)
+linkedList.add(4)
+
+const sorterList = new Sorter(linkedList)
+sorterList.sort()
+linkedList.print()
