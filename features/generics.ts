9186a3b68d9c67dc6b30ea92b6be1207ac4cda0f
hsimao <e087754958@gmail.com>
2020-07-04 21:12:03 +0800

    add: 泛型 generics

---
+++ features/generics.ts
@@ -0,0 +1,83 @@
+// class 泛型（generics）應用場景
+class ArrayOfNumbers {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfString {
+  constructor(public collection: string[]) {}
+
+  get(index: number): string {
+    return this.collection[index];
+  }
+}
+
+// 使用泛型（generics）定義
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+new ArrayOfAnything<string>(["a", "b", "a"]);
+
+new ArrayOfAnything<number>([1, 2, 3]);
+
+// 自動類型推斷
+const yoyo = new ArrayOfAnything(["a", "b", "a"]);
+
+// function 泛型（generics）應用場景
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+// 使用泛型（generics）定義
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<number>([1, 2, 3, 4]);
+printAnything<string>(["a", "b", "c", "d"]);
+
+// 自動類型推斷
+printAnything([1, 2, 3, 4]);
+
+// generic constraints 通用約束
+class Car {
+  print() {
+    console.log("I am a car");
+  }
+}
+
+class House {
+  print() {
+    console.log("I am a house");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars([new Car(), new House()]);
