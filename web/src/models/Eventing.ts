7b2de4477ad13b7380a4efef261aa2ba5e299e54
hsimao <e087754958@gmail.com>
2020-07-05 17:00:09 +0800

    add: 新增快捷調用 get、on、trigger 方法, 改用 array function, 解決 this 指向問題

--- web/src/models/Eventing.ts
+++ web/src/models/Eventing.ts
@@ -7,7 +7,7 @@ type Callback = () => void
 export class Eventing {
   events: EventProps = {}
 
-  on(eventName: string, callback: Callback): void {
+  on = (eventName: string, callback: Callback): void => {
     // 先判斷當 events 內是否已經有相同事件, 若無需給空陣列
     const handlers = this.events[eventName] || []
     handlers.push(callback)
@@ -15,7 +15,7 @@ export class Eventing {
     this.events[eventName] = handlers
   }
 
-  trigger(eventName: string): void {
+  trigger = (eventName: string): void => {
     const handlers = this.events[eventName]
 
     if (!handlers || handlers.length === 0) {
