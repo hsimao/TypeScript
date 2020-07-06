8c540500d5725c407830fbbe6ef0193320426346
hsimao <e087754958@gmail.com>
2020-07-06 19:45:32 +0800

    add: 封裝 Collection class

---
+++ web/src/index.ts
@@ -0,0 +1,12 @@
+import { User, UserProps } from './models/User'
+import { Collection } from './models/Collection'
+
+const collection = new Collection<User, UserProps>('http://localhost:3000/users', (json: UserProps) =>
+  User.buildUser(json)
+)
+
+collection.on('change', () => {
+  console.log('change', collection)
+})
+
+collection.fetch()
