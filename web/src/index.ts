1c7e42a39b1e7dd2b7cb36ec0e4c61a5d0bc204e
hsimao <e087754958@gmail.com>
2020-07-08 21:48:55 +0800

    add: CollectionView、UserList class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,14 +1,33 @@
-import { UserEdit } from './views/UserEdit'
-import { User } from './models/User'
+// import { UserEdit } from './views/UserEdit'
+// import { User } from './models/User'
 
-const user = User.buildUser({ name: 'Mars', age: 30 })
+// const user = User.buildUser({ name: 'Mars', age: 30 })
 
-const root = document.getElementById('root')
+// const root = document.getElementById('root')
 
-if (root) {
-  const userEdit = new UserEdit(root, user)
-  userEdit.render()
-  console.log(userEdit)
-} else {
-  throw new Error('')
-}
+// if (root) {
+//   const userEdit = new UserEdit(root, user)
+//   userEdit.render()
+//   console.log(userEdit)
+// } else {
+//   throw new Error('')
+// }
+
+// render 用戶列表
+import { UserList } from './views/UserList'
+import { Collection } from './models/Collection'
+import { User, UserProps } from './models/User'
+
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+  return User.buildUser(json)
+})
+
+users.on('change', () => {
+  const root = document.getElementById('root')
+
+  if (root) {
+    new UserList(root, users).render()
+  }
+})
+
+users.fetch()
