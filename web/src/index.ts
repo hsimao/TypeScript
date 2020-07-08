d1090a912a7125049ef9a117231c808c8658e476
hsimao <e087754958@gmail.com>
2020-07-08 13:26:20 +0800

    add: UserForm 新增 user model

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,4 +1,7 @@
 import { UserForm } from './views/UserForm'
+import { User } from './models/User'
 
-const userForm = new UserForm(document.getElementById('root'))
+const user = User.buildUser({ name: 'Mars', age: 30 })
+
+const userForm = new UserForm(document.getElementById('root'), user)
 userForm.render()
