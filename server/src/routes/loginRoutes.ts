6ca2482e3a483412dfd11bc9604a4492382ad64f
hsimao <e087754958@gmail.com>
2020-07-20 20:18:42 +0800

    add: login、logout、requireAuth session logic

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,7 +1,16 @@
-import { Router, Request, Response } from 'express'
+import { Router, Request, Response, NextFunction } from 'express'
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined }
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction) {
+  if (req.session?.loggedIn) {
+    next()
+    return
+  }
+
+  res.status(403).send('Not permitted')
 }
 
 const router = Router()
@@ -25,11 +34,39 @@ router.get('/login', (req: Request, res: Response) => {
 router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body
 
-  if (email) {
-    res.send(email.toUpperCase())
+  if (email && password && email === 'hello@gmail.com' && password === 'hello') {
+    req.session = { loggedIn: true }
+    res.redirect('/')
   } else {
-    res.send('You must provide an email')
+    res.send('Invalid email or password')
   }
+})
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session?.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `)
+  } else {
+    res.send(`
+      <div>
+        <div>You are Not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `)
+  }
+})
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = null
+  res.redirect('/')
+})
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Welcome to protected route, logged in user')
 })
 
 export { router }
