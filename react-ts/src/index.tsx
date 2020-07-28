c6cb7c4d5d9ebc77b328d2f18962fc8ad0d849fc
hsimao <e087754958@gmail.com>
2020-07-28 21:38:56 +0800

    add: class and functional component

--- react-ts/src/index.tsx
+++ react-ts/src/index.tsx
@@ -5,10 +5,33 @@ interface AppProps {
   color?: string
 }
 
+// functional component
+// const App = (props: AppProps): JSX.Element => {
+//   return <div>{props.color}</div>
+// }
+
 class App extends React.Component<AppProps> {
+  state = {
+    counter: 0,
+  }
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 })
+  }
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 })
+  }
+
   render() {
-    return <div>Hi there {this.props.color}</div>
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App color='green' />, document.getElementById('root'))
