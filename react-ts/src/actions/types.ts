2ff62c5e9031c4b8c1b5a64a54f9c009c3cd8b99
hsimao <e087754958@gmail.com>
2020-08-01 14:22:32 +0800

    add: 調整 actions 資料結構, 封裝 Action type

--- react-ts/src/actions/types.ts
+++ react-ts/src/actions/types.ts
@@ -1,4 +1,8 @@
+import { FetchTodosAction, DeleteTodoAction } from './todos'
+
 export enum ActionTypes {
   fetchTodos, // 沒有給值會自動帶數字 => 0, 若有其他參數會自動累加
   deleteTodo,
 }
+
+export type Action = FetchTodosAction | DeleteTodoAction
